b05090e875e1d3bf7c34e1980b102a2980177296
MSI\Anna <nysya-2802@gmail.com>
2026-03-14 20:57:19 +0200

    remove comment

--- src/main/java/PasswordGenerator/PasswordGenerator.java
+++ src/main/java/PasswordGenerator/PasswordGenerator.java
@@ -4,31 +4,6 @@ import java.util.Random;
 
 public class PasswordGenerator {
 
-
-//
-//            2.Збереження в локальний репозиторій Maven:
-//
-//    a. Створіть Maven-проект для вашого генератора паролів.
-//
-//    b. Додайте ваш клас PasswordGenerator до цього проекту.
-//
-//    c. Зберіть проект та встановіть його артефакт в локальний репозиторій Maven за допомогою команди mvn install.
-//
-//	3.Підключення та використання у новому проекті:
-//
-//    a. Створіть новий Maven-проект, в якому ви будете використовувати генератор паролів.
-//
-//    b. Додайте залежність до вашого генератора паролів у файлі pom.xml нового проекту.
-//
-//    c. Імпортуйте клас PasswordGenerator у вашому новому проекті та використовуйте його метод для генерації паролів.
-//
-//            4.Перевірка роботи:
-//
-//    a. Створіть об'єкт класу PasswordGenerator у вашому новому проекті та викличте його метод generatePassword для генерації пароля.
-//
-//    b. Перевірте, що згенерований пароль відповідає очікуваній довжині та містить випадкові символи.
-
-
     private static final Random RANDOM = new Random();
     public static final String CAPITAL_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String SMALL_LETTERS = "abcdefghijklmnopqrstuvwxyz";
